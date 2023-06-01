28b0db2ff468584aba551b3b0b132399f192df19
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2023-06-01 11:11:25 +0200

    mongodb remote connection

--- src/db/test.js
+++ src/db/test.js
@@ -3,11 +3,11 @@ const app = express();
 //const connection = require('../confing/db_connection');
 const test_mongo = require('../confing/test_mongo');
 
-const { MongoClient } = require("mongodb");
 require('dotenv').config({path: '../env/developement.env'});
 
 
 app.get('/', (req, res) => {
+    test_mongo.showElements();
 });
 
 app.listen(3000, () => console.log('Server avviato sulla porta 3000'));
