a6e370a6c695fc76327e60ef9aab59bd3f07e1d2
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2023-06-01 11:13:38 +0200

    mongodb remote connection 2

--- src/confing/test_mongo.js
+++ src/confing/test_mongo.js
@@ -1,20 +1,18 @@
 const {MongoClient} = require('mongodb');
 require('dotenv').config({path: '../env/developement.env'});
 
-var url = "mongodb://127.0.0.1:27017/";
-const PAGE_SIZE = 100;
-const db = process.env.DB_NAME;
+const url = process.env.DB_URI;
+const db_name = process.env.DB_NAME;
 const collection = "reparti";
 
 exports.showElements = function() {
-     var url = "mongodb://127.0.0.1:27017/";
+     
      const client = new MongoClient(url);
      async function main() {
           try {
                await client.connect();
-
                //await listDatabases(client);
-               const reparti = await client.db("sami_db").collection("reparti");
+               const reparti = await client.db(db_name).collection("reparti");
                const cursor = reparti.find({});
 
                for await (const doc of cursor) {
