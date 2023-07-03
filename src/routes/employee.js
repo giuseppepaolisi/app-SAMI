f1977970b24ea7ad377a561ff74a8fef7378d541
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2023-07-03 15:04:13 +0200

    add link to insert

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const employee = require('../controller/employeeController');
-
-const bodyparse = require("body-parser");
+const {verifyToken} = require('../middleware/user-auth');
 
 
 //permette di visualizzare la lista di reparti disponibili
-router.get('/reparti',(req, res) => {
+router.get('/reparti', verifyToken, (req, res) => {
   res.render('dipendente/reparti.ejs');
 });
 
 //permette di selezionare il reparto di riferimento
-router.get('/reparto/:reparto/', (req, res) => {
+router.get('/reparto/:reparto/', verifyToken, (req, res) => {
     console.log("\n\n" + req.params.reparto);
     switch(req.params.reparto) {
         case "produzione":
@@ -31,27 +30,27 @@ router.get('/reparto/:reparto/', (req, res) => {
 });
 
 //permette di visualizzare la lista di macchine dopo aver selezionato reparto e tipologia di molla
-router.get('/macchine/:reparto/:tipo/', (req, res) => {
+router.get('/macchine/:reparto/:tipo/', verifyToken, (req, res) => {
     console.log("\n\nMACCHINE");
     employee.getMacchine(req, res);
   });
 
   //permette di visualizzare la lista di macchine dopo aver selezionato il reparto assemblaggio
-  router.get('/macchine/:reparto/', (req, res) => {
+  router.get('/macchine/:reparto/', verifyToken, (req, res) => {
     console.log("\n\nMACCHINE");
     employee.getMacchine(req, res);
   });
 
   //permette di visualizzare il form di inserimento dopo aver selezionato reparto, tipo molla e macchina
-  router.get('/inserisci/:reparto/:tipo/:macchina', (req, res) => {
+  router.get('/inserisci/:reparto/:tipo/:macchina', verifyToken, (req, res) => {
     console.log("\n\nInserisci " + req.params.reparto + "\n"+ req.params.tipo + "\n" + req.params.macchina);
-    res.render('', {reparto : req.params.reparto, tipo: req.params.tipo});
+    res.render('insert.ejs', {reparto : req.params.reparto, tipo: req.params.tipo, macchina:req.params.macchina});
   });
 
   //permette di visualizzare il form di inserimento dopo aver selezionato reparto e macchina
-  router.get('/inserisci/:reparto/:macchina', (req, res) => {
+  router.get('/inserisci/:reparto/:macchina', verifyToken, (req, res) => {
     console.log("\n\nInserisci " + req.params.reparto);
-    res.render('', {reparto : req.params.reparto});
+    res.render('insert.ejs', {reparto : req.params.reparto, macchina:req.params.macchina});
   });
 
 
