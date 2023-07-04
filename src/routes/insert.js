56df2e19ae79093feb90821abfcfce99957760df
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2023-07-04 19:43:10 +0200

    fix insert.ejs form

--- src/routes/insert.js
+++ src/routes/insert.js
@@ -3,10 +3,6 @@ const router = express.Router();
 const verifyToken = require('../middleware/user-auth');
 const repartiController = require('../controller/repartiController');
 
-router.get('/insert', (req, res) => {
-  res.render('insert.ejs');
-});
-
 /* POST send data to db*/
 router.post('/insert', (req, res) => {
   console.log('inizio insert POST');
