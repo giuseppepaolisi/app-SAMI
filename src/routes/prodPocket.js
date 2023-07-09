4454142e4fe993641709b07ab8292f0e2f02b0d4
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2023-07-09 13:14:05 +0200

    added dinamic table content

--- src/routes/prodPocket.js
+++ src/routes/prodPocket.js
@@ -48,8 +48,14 @@ router.get('/prodPocket/:reparto/:tipo?', async function(req, res, next) {
       //const list = await Reparti.find({tipo:'Produzione', reparto:'produzione'}).exec(); 
       const aheader = ['tipo', 'reparto', 'diametro', 'portata','peso', 'data','ora','macchina', 'quantita','h_lavorate','h_fermo', 'user', 'diamtero_filo','diamtero_molla','giri_molla','file'];
       /*const aheader = "nome";*/
+
+      //prende gli header della prima occorrenza nel db ed elimina alcuni di loro.
+      const keys = Object.keys(list[0]._doc).filter(key => !['_id', '__v', 'tipo', 'reparto'].includes(key));
+
+      console.log(keys); 
+      console.log(list[0]);
     
-      res.render('prodPocket', { title:reparto + " " +tipo,aheader:aheader,list:list});
+      res.render('prodPocket', { title:reparto + " " +tipo,aheader:keys,list:list});
     
     }
 
