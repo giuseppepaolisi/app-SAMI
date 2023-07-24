cf5bb0c6a5e7e42222f50a167ac82f1a42952fc2
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2023-07-24 16:00:43 +0200

    fix editUser

--- src/routes/users.js
+++ src/routes/users.js
@@ -41,4 +41,49 @@ router.delete('/eliminaDipendente/:id', async (req, res, next) => {
       });
 });
 
+router.get('/addUser', async function(req, res, next) {
+
+
+  // Renderizza la pagina del calendario utilizzando il file "addUser.ejs"
+  res.render('addUser');
+});
+
+router.get('/editUser/:id', async function(req, res, next) {
+
+  const uri = process.env.DB_URI || "";
+  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  const user = await User.find({_id: req.params.id}).exec(); 
+
+  console.log(user.nome + "ECCOMIIIII");
+
+  // Renderizza la pagina del calendario utilizzando il file "addUser.ejs"
+  res.render('editUser', {user:user[0]});
+});
+
+router.post('/editUser/:id', async function(req, res, next) {
+
+  const elementId = req.params.id;
+  console.log(elementId);
+    const uri = process.env.DB_URI || "";
+    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    const editingUser = {
+      nome : req.body.nome,
+      cognome : req.body.cognome,
+      password : req.body.password
+    };
+    User.findByIdAndUpdate(elementId, editingUser, { new: true, runValidators: true })
+      .then((editingUser) => {
+        if(editingUser) {
+          console.log('Elemento aggiornato:', editingUser);
+          res.redirect('/dipendenti');
+        } else {
+          console.log('Elemento non trovato.');
+        }
+      }).catch((errore) => {
+        console.error('Errore durante l\'aggiornamento dell\'elemento:', errore);
+        //res.status(500).json({ message: "Errore durante l'eliminazione dell'elemento" });
+      });
+
+});
+
 module.exports = router;
