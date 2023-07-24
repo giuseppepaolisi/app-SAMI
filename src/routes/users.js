3fde078e8ee9258c387dc2fb7ef934b6f2e21ada
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2023-07-24 16:42:56 +0200

    added commet in user.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,6 +4,7 @@ const employee = require('../controller/employeeController');
 const { verifyToken, isAdmin, signToken, deleteToken } = require('../middleware/user-auth');
 const mongoose = require('mongoose');
 const User = require("./../db/user.js");
+const employeeController = require('../controller/employeeController');
 
 /* GET users listing. */
 router.get('/', isAdmin, function(req, res, next) {
@@ -14,40 +15,24 @@ router.get('/lista', async function(req, res, next) {
   employee.getEmployees(req, res,next);
 }); 
 
+//permette di aggiungere un dipendente al sistema
 router.post('/addUser', async (req, res, next) => {
   employee.addEmployee(req, res, next);
 
 });
 
-router.delete('/eliminaDipendente/:id', async (req, res, next) => {
-  const elementId = req.params.id;
-  console.log(elementId);
-    const uri = process.env.DB_URI || "";
-    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    const flag = {
-      deleted : 1
-    };
-    User.findByIdAndUpdate(elementId, flag, { new: true, runValidators: true })
-      .then((flag) => {
-        if(flag) {
-          console.log('Elemento aggiornato:', flag);
-          res.json({ message: "Elemento eliminato con successo" });
-        } else {
-          console.log('Elemento non trovato.');
-        }
-      }).catch((errore) => {
-        console.error('Errore durante l\'aggiornamento dell\'elemento:', errore);
-        res.status(500).json({ message: "Errore durante l'eliminazione dell'elemento" });
-      });
-});
-
+//permette di visualizzare la pagina per aggiungere un nuovo dipendente
 router.get('/addUser', async function(req, res, next) {
-
-
   // Renderizza la pagina del calendario utilizzando il file "addUser.ejs"
   res.render('addUser');
 });
 
+//permette di cancellare un dipendente dal sistema
+router.delete('/eliminaDipendente/:id', async (req, res, next) => {
+  employeeController.deleteEmployee(req, res, next);
+});
+
+//permette di visualizzare la pagina di modifica dati per un untente
 router.get('/editUser/:id', async function(req, res, next) {
 
   const uri = process.env.DB_URI || "";
@@ -60,6 +45,7 @@ router.get('/editUser/:id', async function(req, res, next) {
   res.render('editUser', {user:user[0]});
 });
 
+//permette di modificare i dati riguardanti un untente
 router.post('/editUser/:id', async function(req, res, next) {
 
   const elementId = req.params.id;
