69d007ad0ad2c914bf7be7d79d079da363c6fc7c
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2024-11-05 21:26:02 +0100

    Fix

--- src/routes/cliente.js
+++ src/routes/cliente.js
@@ -5,12 +5,12 @@ const { isAdmin  } = require('../middleware/user-auth');
 
 const router = express.Router();
 
-
+// Controller route
 router.get('/showCliente', isAdmin, clienteController.getClienti);
 router.post('/addCliente', isAdmin, clienteController.addCliente);
 router.delete('/eliminaCliente/:id', isAdmin, clienteController.deleteCliente);
 
-// Render ADD
+// Template route
 router.get('/addCliente', isAdmin, (req, res) => {
     res.render('addCliente', { title: 'Aggiungi Cliente' });
 });
