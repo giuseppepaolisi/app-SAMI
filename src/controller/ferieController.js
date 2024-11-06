fb1b5f4a762076d8cea0494344884f9c5eab66d4
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2024-11-06 18:14:45 +0100

    Add comments ferie

--- src/controller/ferieController.js
+++ src/controller/ferieController.js
@@ -2,89 +2,160 @@ const Ferie = require("../model/ferie");
 const User = require("../model/user");
 const moment = require('moment');
 
+/**
+ * Restituisce i dati di tutti i mesi per un anno specificato.
+ * @param {number} year - L'anno per cui generare i dati dei mesi.
+ * @returns {Array} Un array contenente i dati di ogni mese, inclusi i giorni e i nomi dei giorni.
+ */
 function getAllMonths(year) {
-    const monthsData = [];
-    for (let month = 0; month < 12; month++) {
-      const daysInMonth = [];
-      const firstDayOfMonth = moment({ year, month, day: 1 });
-      const daysInMonthCount = firstDayOfMonth.daysInMonth();
-      for (let i = 1; i <= daysInMonthCount; i++) {
-        const date = moment(firstDayOfMonth).date(i);
-        daysInMonth.push({
-          dayOfMonth: i,
-          dayOfWeek: date.format('dddd'),
-        });
-      }
-      monthsData.push({
-        month: firstDayOfMonth.format('MMMM'),
-        year,
-        daysInMonth,
+  const monthsData = [];
+  for (let month = 0; month < 12; month++) {
+    const daysInMonth = [];
+    const firstDayOfMonth = moment({ year, month, day: 1 });
+    const daysInMonthCount = firstDayOfMonth.daysInMonth();
+
+    for (let i = 1; i <= daysInMonthCount; i++) {
+      const date = moment(firstDayOfMonth).date(i);
+      daysInMonth.push({
+        dayOfMonth: i,
+        dayOfWeek: date.format('dddd'),
       });
     }
-    return monthsData;
+
+    monthsData.push({
+      month: firstDayOfMonth.format('MMMM'),
+      year,
+      daysInMonth,
+    });
   }
+  return monthsData;
+}
 
 const ferieController = {
+  
+  /**
+   * Mostra la pagina per modificare i dati di una richiesta ferie per un dipendente.
+   * @param {Object} req - La richiesta Express.
+   * @param {Object} res - La risposta Express.
+   */
   getEditFeriePage: async (req, res, next) => {
     const ferie = await Ferie.findById(req.params.id);
     res.render('editFerie', { ferie });
   },
 
+  /**
+   * Aggiorna di una richiesta di ferie per un dipendente.
+   * @param {Object} req - La richiesta Express contenente i nuovi dati.
+   * @param {Object} res - La risposta Express.
+   */
   updateFerie: async (req, res, next) => {
     const { dataInizio, dataFine, isFerie } = req.body;
-    const ferie = await Ferie.findByIdAndUpdate(req.params.id, { dataInizio, dataFine, tipologia:isFerie }, { new: true, runValidators: true });
-    if (ferie) {
-      res.redirect('/showFerie');
-    } else {
-      console.error('Errore durante l\'aggiornamento dell\'elemento:', errore);
+    try {
+      const ferie = await Ferie.findByIdAndUpdate(req.params.id, 
+      { dataInizio, dataFine, tipologia: isFerie }, 
+      { new: true, runValidators: true });
+      if (ferie) {
+        res.redirect('/showFerie');
+      }
+    } catch (error) {
+      console.error('Errore durante l\'aggiornamento dell\'elemento:', error);
     }
   },
 
+  /**
+   * Mostra la pagina per aggiungere una nuova richiesta ferie.
+   * @param {Object} req - La richiesta Express.
+   * @param {Object} res - La risposta Express.
+   */
   getAddFeriePage: async (req, res, next) => {
     const options = await User.find({ deleted: 0 });
     res.render('addFerie', { options });
   },
 
+  /**
+   * Aggiunge una nuova richiesta ferie al database.
+   * @param {Object} req - La richiesta Express con i dati della nuova richiesta.
+   * @param {Object} res - La risposta Express.
+   */
   addFerie: async (req, res, next) => {
     const { opzione, dataInizio, dataFine, isFerie, noteFerie } = req.body;
-    const user = await User.findOne({ user: opzione });
-    if (!user) {
-      return res.status(400).send('Utente non trovato');
+    try {
+      const user = await User.findOne({ user: opzione });
+      if (!user) {
+        return res.status(400).send('Utente non trovato');
+      }
+      const ferie = new Ferie({
+        nome: user.nome,
+        cognome: user.cognome,
+        dataInizio,
+        dataFine,
+        tipologia: isFerie,
+        note: noteFerie,
+        deleted: 0
+      });
+      await ferie.save();
+      res.redirect('/conferma');
+    } catch (error) {
+      console.error("Errore durante l'inserimento della richiesta ferie:", error);
     }
-    const ferie = new Ferie({
-      nome: user.nome,
-      cognome: user.cognome,
-      dataInizio,
-      dataFine,
-      tipologia: isFerie,
-      note: noteFerie,
-      deleted: 0
-    });
-    await ferie.save();
-    res.redirect('/conferma');
   },
 
+  /**
+   * Elimina una richiesta ferie.
+   * @param {Object} req - La richiesta Express contenente l'ID dell'elemento.
+   * @param {Object} res - La risposta Express.
+   */
   deleteFerie: async (req, res, next) => {
-    const ferie = await Ferie.findByIdAndUpdate(req.params.id, { deleted: 1 }, { new: true });
-    if (ferie) {
-      res.json({ message: "Elemento eliminato con successo" });
-    } else {
+    try {
+      const ferie = await Ferie.findByIdAndUpdate(req.params.id, { deleted: 1 }, { new: true });
+      if (ferie) {
+        res.json({ message: "Elemento eliminato con successo" });
+      } else {
+        res.status(500).json({ message: "Errore durante l'eliminazione dell'elemento" });
+      }
+    } catch (error) {
+      console.error("Errore durante l'eliminazione della richiesta ferie:", error);
       res.status(500).json({ message: "Errore durante l'eliminazione dell'elemento" });
     }
   },
 
+  /**
+   * Mostra tutte le ferie in formato tabella.
+   * @param {Object} req - La richiesta Express.
+   * @param {Object} res - La risposta Express.
+   */
   showFerie: async (req, res, next) => {
-    const list = await Ferie.find({ deleted: 0 });
-    const aheader = ['nome', 'cognome', 'dataInizio', 'dataFine', 'tipologia', 'note'];
-    res.render('tableFerie', { title: 'Ferie', aheader, list, moment: moment });
+    try {
+      const list = await Ferie.find({ deleted: 0 });
+      const aheader = ['nome', 'cognome', 'dataInizio', 'dataFine', 'tipologia', 'note'];
+      res.render('tableFerie', { title: 'Ferie', aheader, list, moment });
+    } catch (error) {
+      console.error("Errore nel recupero delle richieste ferie:", error);
+      res.redirect('/');
+    }
   },
 
+  /**
+   * Mostra il calendario annuale con tutte le ferie.
+   * @param {Object} req - La richiesta Express.
+   * @param {Object} res - La risposta Express.
+   */
   getCalendarPage: async (req, res, next) => {
-    const options = await Ferie.find({ deleted: 0 });
-    const monthsData = getAllMonths(moment().year());
-    res.render('calendar', { year: moment().year(), months: monthsData, title: 'Calendario', options });
+    try {
+      const options = await Ferie.find({ deleted: 0 });
+      const monthsData = getAllMonths(moment().year());
+      res.render('calendar', { year: moment().year(), months: monthsData, title: 'Calendario', options });
+    } catch (error) {
+      console.error("Errore nel caricamento della pagina calendario:", error);
+      res.redirect('/');
+    }
   },
 
+  /**
+   * Mostra la pagina di conferma dopo l'aggiunta di una richiesta ferie.
+   * @param {Object} req - La richiesta Express.
+   * @param {Object} res - La risposta Express.
+   */
   getConfirmaPage: async (req, res, next) => {
     res.render('conferma');
   }
